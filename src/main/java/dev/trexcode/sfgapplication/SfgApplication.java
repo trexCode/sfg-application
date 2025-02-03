a0ab361ddd6993d0c02861d346bb21b641e85800
dlopezd <ingldurandaniel@gmail.com>
2025-02-03 13:11:34 -0600

    First commit

---
+++ src/main/java/dev/trexcode/sfgapplication/SfgApplication.java
@@ -0,0 +1,13 @@
+package dev.trexcode.sfgapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/trexcode/sfgapplication/SfgApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.trexcode.sfgapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
